9c979a673e543a72de9586bb1f7467abf089ed2c
jhklvdblre <frogra10@nith.no>
2012-09-22 18:37:13 +0200

    Changed name of map to open

--- Open eggtest in editor.bat
+++ Open eggtest in editor.bat
@@ -1,7 +1,7 @@
 @echo off
 C:\UDK\UDK-2012-07\Binaries\Win32\UDK.com make
 IF errorlevel 1 GOTO :ERROR_EXIT
-START C:\UDK\UDK-2012-07\Binaries\Win32\UDK.exe editor MagicalTestMap.udk
+START C:\UDK\UDK-2012-07\Binaries\Win32\UDK.exe editor eggtest.udk
 GOTO :GOOD_EXIT
 :ERROR_EXIT
 pause
